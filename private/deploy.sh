ebe880ccd6917cda3377e490aa038de8ad890abf
Noland <noland.germain@gmail.com>
2018-01-18 22:26:21 -0800

    arrgghhh

--- private/deploy.sh
+++ private/deploy.sh
@@ -1,4 +1,6 @@
 sudo pm2 stop all
+source /home/noland/.nvm/nvm.sh
+nvm use 8.9.3
 METEOR_PACKAGE_DIRS=/var/www/development/capr/vulcan/packages meteor build --directory /var/www/development/builds
 cd /var/www/development/builds/bundle/programs/server
 /home/noland/.nvm/versions/node/v8.9.3/bin/npm install
