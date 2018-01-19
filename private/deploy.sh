64edddce293cab65738bcbfa39fb7d8884668be7
Noland <noland.germain@gmail.com>
2018-01-18 18:01:01 -0800

    Tweaks

--- private/deploy.sh
+++ private/deploy.sh
@@ -1,5 +1,5 @@
 sudo pm2 stop all
-METEOR_PACKAGE_DIRS=\"/var/www/development/capr/vulcan/packages\" meteor build --directory /var/www/development/builds
+METEOR_PACKAGE_DIRS=/var/www/development/capr/vulcan/packages meteor build --directory /var/www/development/builds
 cd /var/www/development/builds/bundle/programs/server
 npm install
 sudo pm2 restart all
