696f4a0b60125c6818ee00aea2e2342664dd0652
Noland <noland.germain@gmail.com>
2018-02-09 12:00:35 -0800

    Peachy

--- private/deploy.sh
+++ private/deploy.sh
@@ -1,9 +1,17 @@
 sudo pm2 stop all
+
 source /home/noland/.nvm/nvm.sh
 nvm use 8.9.4
+
+cd /var/www/development/capr/vulcan/
+npm install
+cd /var/www/development/capr/capr-website/
+npm install
+
 METEOR_PACKAGE_DIRS=/var/www/development/capr/vulcan/packages meteor build --directory /var/www/development/capr/builds
 cd /var/www/development/capr/builds/bundle/programs/server
 /home/noland/.nvm/versions/node/v8.9.3/bin/npm install
+
 sudo pm2 restart all
 
 # To fix shit?
