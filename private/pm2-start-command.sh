479a27e0d15e9a4c93a9f6769373e205f406fad0
Noland <noland.germain@gmail.com>
2018-03-30 19:10:18 -0400

    peachy

--- private/pm2-start-command.sh
+++ private/pm2-start-command.sh
@@ -2,7 +2,8 @@ sudo \
     MONGO_URL=mongodb://localhost:27017/capr \
     PORT=2015 \
     ROOT_URL=https://climateaction.fun \
+    METEOR_SETTINGS="$(cat settings.json)" \
 pm2 start \
   /var/www/development/capr/builds/bundle/main.js \
-  --name capr-website
+  --name capr-website \
   --interpreter /home/noland/.nvm/versions/node/v8.9.3/bin/node
---
+++ private/start-dev.sh
@@ -0,0 +1,4 @@
+METEOR_PACKAGE_DIRS="../:../vulcan/packages" \
+ROOT_URL="https://climateaction.fun" \
+meteor --settings settings.json --port 2015
+
