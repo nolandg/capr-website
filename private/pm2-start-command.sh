d8a865be27fb29af697956eb0f84e964699ab159
Noland <noland.germain@gmail.com>
2018-03-30 18:22:27 -0700

    Peachy

--- private/pm2-start-command.sh
+++ private/pm2-start-command.sh
@@ -1,7 +1,7 @@
 sudo \
     MONGO_URL=mongodb://localhost:27017/capr \
     PORT=2015 \
-    ROOT_URL=https://climateaction.fun \
+    ROOT_URL=https://climateactionpowellriver.earth \
     METEOR_SETTINGS="$(cat settings.json)" \
 pm2 start \
   /var/www/development/capr/builds/bundle/main.js \
