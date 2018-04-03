9e92484a8d70688b700d3a7003552940d5f6d68e
Noland <noland.germain@gmail.com>
2018-04-03 11:27:08 -0700

    Peachy

--- packages/core/lib/server/main.js
+++ packages/core/lib/server/main.js
@@ -1,7 +1,45 @@
 import { getSetting } from 'meteor/vulcan:lib';
 
 export * from '../modules/index.js';
-console.log('Google: ', getSetting('googleOAuthClientId'));
+
+const greet = (welcomeMsg) => {
+  return function(user, url) {
+    const greeting = (user.profile && user.profile.name) ? (`Hello ${user.profile.name},`) : 'Hello,';
+    return `
+${greeting}
+
+${welcomeMsg}, simply click the link below.
+${url}
+
+Thanks.
+~ The CAPR Team
+`;
+  };
+}
+
+Accounts.emailTemplates = {
+  from: 'CAPR no-reply <no-reply@climateactionpowellriver.earth>',
+  siteName: 'Climate Action Powell River',
+
+  resetPassword: {
+    subject: (user) => {
+      return 'How to reset your CAPR password';
+    },
+    text: greet('To reset your password'),
+  },
+  verifyEmail: {
+    subject: (user) => {
+      return 'How to verify email address';
+    },
+    text: greet('To verify your account email'),
+  },
+  enrollAccount: {
+    subject: function(user) {
+      return 'An CAPR account has been created for you.';
+    },
+    text: greet('To start using the service.')
+  }
+};
 
 ServiceConfiguration.configurations.upsert(
   { service: 'google' },
